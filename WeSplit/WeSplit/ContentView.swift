1d2264950dbe1b5f72c3c58ae5248d7205d35532
Sandeep Hegde <sandeep_hegde@outlook.com>
2023-08-13 01:04:27 +0530

    Updated UI with tip percentage calculation

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -5,21 +5,74 @@
 //  Created by Sandeep Hegde on 12/08/23.
 //
 
-// Importing the SwiftUI framework to utilize its features.
+
 import SwiftUI
 
-// Defining a ContentView struct that conforms to the View protocol from SwiftUI.
+
 struct ContentView: View {
-    // The body computed property returns the view's content for rendering.
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+
     var body: some View {
-        // Creating a text view with the content "Hello, world!".
-        Text("Hello, world!")
-            // Adding padding around the text view.
-            .padding()
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD" ))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave?")
+                }
+                
+                Section {
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
     }
 }
 
-// Provides a preview of the ContentView in Xcode's canvas.
+
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
