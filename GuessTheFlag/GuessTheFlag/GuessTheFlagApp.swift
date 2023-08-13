2cd6dd5696547166e11ce17505ba444935ee3e12
Sandeep Hegde <sandeep_hegde@outlook.com>
2023-08-13 16:18:03 +0530

    Initial commit

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sandeep Hegde on 13/08/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
