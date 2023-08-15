372bf297f3c59e1f792eec3326bfb81686ca91fe
Sandeep Hegde <sandeep_hegde@outlook.com>
2023-08-15 17:06:43 +0530

    Initial commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Sandeep Hegde on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
