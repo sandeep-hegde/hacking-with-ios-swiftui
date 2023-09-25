4ebeb0d48937834a822612eea47cc7350c5278b9
Sandeep Hegde <sandeep_hegde@outlook.com>
2023-09-25 18:29:28 +0530

    Building a list that can be deleted

---
+++ iExpense/iExpense/ExpenseItem.swift
@@ -0,0 +1,8 @@
+//
+//  ExpenseItem.swift
+//  iExpense
+//
+//  Created by Sandeep Hegde on 25/09/23.
+//
+
+import Foundation
---
+++ iExpense/iExpense/Expenses.swift
@@ -0,0 +1,8 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Sandeep Hegde on 25/09/23.
+//
+
+import Foundation
