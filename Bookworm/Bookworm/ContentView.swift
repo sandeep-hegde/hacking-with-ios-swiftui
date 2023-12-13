de1bbea4a60c610861d517ecf21b1dfad2c169d9
Sandeep Hegde <sandeep_hegde@outlook.com>
2023-12-13 12:41:37 +0530

    Initial commit

---
+++ Bookworm/Bookworm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bookworm
+//
+//  Created by Sandeep Hegde on 13/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
