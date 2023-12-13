39381d45126d73e94354db7c852e005746db0e6e
Sandeep Hegde <sandeep_hegde@outlook.com>
2023-12-13 13:46:17 +0530

    Creating Book Data Model, Adding and configuring SwiftData and, Adding Add book view

---
+++ Bookworm/Bookworm/AddBookView.swift
@@ -0,0 +1,18 @@
+//
+//  AddBookView.swift
+//  Bookworm
+//
+//  Created by Sandeep Hegde on 13/12/23.
+//
+
+import SwiftUI
+
+struct AddBookView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddBookView()
+}
---
+++ Bookworm/Bookworm/Book.swift
@@ -0,0 +1,8 @@
+//
+//  Book.swift
+//  Bookworm
+//
+//  Created by Sandeep Hegde on 13/12/23.
+//
+
+import Foundation
