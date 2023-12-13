d8ba5694abb0c6423d2579010bfd820909d931b3
Sandeep Hegde <sandeep_hegde@outlook.com>
2023-12-13 13:47:56 +0530

    Creating Book Data Model, Adding & Configuring SwiftData, Creating Add Book View

--- Bookworm/Bookworm/Book.swift
+++ Bookworm/Bookworm/Book.swift
@@ -6,3 +6,22 @@
 //
 
 import Foundation
+import SwiftData
+
+// Book Data Model
+@Model
+class Book {
+    var title: String
+    var author: String
+    var genre: String
+    var review: String
+    var rating: Int
+    
+    init(title: String, author: String, genre: String, review: String, rating: Int) {
+        self.title = title
+        self.author = author
+        self.genre = genre
+        self.review = review
+        self.rating = rating
+    }
+}
